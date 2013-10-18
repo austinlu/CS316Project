a964ddbd4ca41dbcf9546bd1727a3be990e8f4ce
Peggy Li <peggy.li@duke.edu>
2013-10-18 02:09:49 +0000

    Added and fixed queries

--- test-sample.sql
+++ test-sample.sql
@@ -1,29 +1,33 @@
--- select the names of all counties in a particular state --
+-- select the names of all counties in a particular state
+-- used for populating the dropdown menu where user selects location
 SELECT name
 FROM County
-WHERE state = 'California';
+WHERE state = 'CA'
+ORDER BY name asc;
 
--- select top 10 CO-polluting facilities 
+-- select names of top 10 CO-polluting facilities 
 -- within a particular county and state
-SELECT *
-FROM Facility
-WHERE county = 'Durham'
+SELECT Facility.name
+FROM Facility, FacilityPollution
+WHERE Facility.EIS_ID = FacilityPollution.EIS_ID 
+AND county = 'Durham'
 AND state = 'NC'
 ORDER BY carbon_monoxide DESC
 LIMIT 10;
 
-
 -- select aggregate pollution data and demographics data
 -- for a particular county
 SELECT County.name, County.state, SUM(carbon_monoxide), SUM(oxides_of_nitrogen), SUM(sulfur_dioxide), SUM("particulate_matter_2.5"), SUM(particulate_matter_10), SUM(lead), SUM(mercury), population_density, unemployment_rate, median_income, percent_HS, percent_bachelors
-FROM Facility, County 
+FROM County, Facility, FacilityPollution
 WHERE Facility.county = County.name
 AND Facility.state = County.state
-AND County.name = 'Durham'
+AND Facility.EIS_ID = FacilityPollution.EIS_ID
+GROUP BY County.name, County.state
+HAVING County.name = 'Durham'
 AND County.state = 'NC';
 
 -- select the names of all counties with unemployment rate above 8%
--- and that contain at least one facility that emits 100+ tons of lead
+-- and that contain at least one facility that emits 10+ tons of lead
 SELECT name, state
 FROM County
 WHERE unemployment_rate > 8.0
@@ -32,7 +36,7 @@ AND EXISTS (
 	WHERE Facility.EIS_ID = FacilityPollution.EIS_ID
 	AND Facility.county = County.name
 	AND Facility.state = County.state
-	AND FacilityPollution.lead > 100.0
+	AND FacilityPollution.lead > 10.0
 );
 
 
