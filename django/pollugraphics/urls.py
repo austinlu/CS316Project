baa366e0e30b30ca7fbd00231cac3916a0ea23f1
Peggy Li <peggy.li@duke.edu>
2013-11-12 13:39:53 -0500

    Added county and facility urls for pollugraphics

--- django/pollugraphics/urls.py
+++ django/pollugraphics/urls.py
@@ -3,4 +3,6 @@ from pollugraphics import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index')
+	url(r'^county$', views.county, name='county')
+	url(r'^facility$', views.facility, name='facility')
 )
