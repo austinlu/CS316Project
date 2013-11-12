90b31a2415477d08a3deb01c291d89235126d161
Peggy Li <peggy.li@duke.edu>
2013-11-12 17:07:50 -0500

    Moved setup script to project home directory

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ dropdb pollugraphics
 createdb pollugraphics
 
 # create database tables
-python manage.py syncdb
+python django/manage.py syncdb
 
 # load data into tables
-psql -af "COPY state FROM '${PWD}/../data/stateTable.csv' DELIMTER ',' CSV"
-psql -af "COPY county FROM '${PWD}/../data/countyTable.csv' DELIMTER ',' CSV"
-psql -af "COPY facility FROM '${PWD}/../data/facilityTable.csv' DELIMTER ',' CSV"
-psql -af "COPY facilitypollution FROM '${PWD}/../data/facilityPollutionTable.csv' DELIMTER ',' CSV"
+psql -af "COPY state FROM '${PWD}data/stateTable.csv' DELIMTER ',' CSV"
+psql -af "COPY county FROM '${PWD}data/countyTable.csv' DELIMTER ',' CSV"
+psql -af "COPY facility FROM '${PWD}data/facilityTable.csv' DELIMTER ',' CSV"
+psql -af "COPY facilitypollution FROM '${PWD}data/facilityPollutionTable.csv' DELIMTER ',' CSV"
