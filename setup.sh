5222170959892c731a5eb9334008db99acee733d
Peggy Li <peggy.li@duke.edu>
2013-11-12 17:04:11 -0500

    Created setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create new database
+dropdb pollugraphics
+createdb pollugraphics
+
+# create database tables
+python manage.py syncdb
+
+# load data into tables
+psql -af "COPY state FROM '${PWD}/../data/stateTable.csv' DELIMTER ',' CSV"
+psql -af "COPY county FROM '${PWD}/../data/countyTable.csv' DELIMTER ',' CSV"
+psql -af "COPY facility FROM '${PWD}/../data/facilityTable.csv' DELIMTER ',' CSV"
+psql -af "COPY facilitypollution FROM '${PWD}/../data/facilityPollutionTable.csv' DELIMTER ',' CSV"
